13712e74c7a00b1e1a06303dea31d7263a7be8c3
Jacqui Grimes <grimesjacqui@gmail.com>
2017-10-11 20:05:38 +0100

    Adding dynamoDB database

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 const Alexa = require('alexa-sdk')
+const awsSDK = require('aws-sdk')
+const docClient = new awsSDK.DynamoDB.DocumentClient()
 const HELP_MESSAGE = 'Try saying: Alexa, ask Super Calories Checker how many calories are there in butter'
 const STOP_MESSAGE = 'Goodbye!'
 const APP_ID = 'amzn1.ask.skill.944f22eb-da6b-4090-8ec6-cc20dda7342f'
+const foodItemsTable = 'foodItems'
 
 exports.handler = function (event, template, context) {
   const alexa = Alexa.handler(event, template, context)
   alexa.APP_ID = APP_ID
   alexa.registerHandlers(handlers)
   alexa.execute()
-}
-
-const foodItems = {
-  butter: {
-    calories: 717,
-    per: '100 grams'
-  }
 }
 
 const handlers = {
@@ -36,11 +32,20 @@ const handlers = {
   GetCaloriesIntent () {
     const foodItem = this.event.request.intent.slots.FoodItem.value
 
-    if (foodItem && foodItem in foodItems) {
-      const nutrition = foodItems[foodItem]
-      this.emit(':tell', `There are ${nutrition.calories} calories in ${nutrition.per} of ${foodItem}`)
-    } else {
-      this.emit(':tell', `Sorry I couldn't find that item in my database.`)
-    }
+    const self = this
+
+    docClient.get({
+      TableName: foodItemsTable,
+      Key: {
+        name: foodItem
+      }
+    }, function (error, result) {
+      if (error || !result) {
+        self.emit(':tell', `Sorry, I couldn't find that item in my database`)
+      } else {
+        const nutrition = result.Item
+        self.emit(':tell', `There are ${nutrition.calories} calories in ${nutrition.per} of ${foodItem}`)
+      }
+    })
   }
 }
